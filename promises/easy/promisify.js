817d440a53000927e3af133c86cd657cadb4ae51
Sourav Saha <sourav024saha@gmail.com>
2026-02-25 13:26:05 +0530

    Update promisify.js

--- promises/easy/promisify.js
+++ promises/easy/promisify.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 function promisify(fn) {
   return function (...args) {
     return new Promise((resolve, reject) => {
@@ -31,9 +31,6 @@ function promisify(fn) {
 // // 1. Resolve with result if callback gets (null, result)
 // // 2. Reject if callback gets an error
 
-function promisify(fn) {
 
->>>>>>> 503324ff797fa1fc9e35ec3d9a6fd4f67a90c954
-}
 
 module.exports = promisify;
