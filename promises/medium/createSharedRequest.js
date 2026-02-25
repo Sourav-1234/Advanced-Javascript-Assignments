f80b64bd37faaee0c2d14ebf96a646163ebd0fdf
Sourav Saha <sourav024saha@gmail.com>
2026-02-25 13:41:16 +0530

    Update createSharedRequest.js

--- promises/medium/createSharedRequest.js
+++ promises/medium/createSharedRequest.js
@@ -7,7 +7,7 @@
 // The first call should trigger apiCallFn.
 // If called again while the request is still pending, return the same promise.
 // Once it resolves or rejects, the next call should start a new request.
-<<<<<<< HEAD
+
 function createSharedRequest(apiCallFn) {
   let inFlightPromise = null;
 
@@ -23,8 +23,6 @@ function createSharedRequest(apiCallFn) {
     return inFlightPromise;
   };
 }
-=======
-function createSharedRequest(apiCallFn) { }
->>>>>>> 503324ff797fa1fc9e35ec3d9a6fd4f67a90c954
+
 
 module.exports = createSharedRequest;
